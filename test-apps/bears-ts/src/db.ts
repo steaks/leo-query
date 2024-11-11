069c921ed10558ca041d87a353c0c3856a326fa0
Steven Wexler <steven.m.wexler@gmail.com>
2024-11-11 00:03:01 -0700

    Update build and create test-apps

---
+++ test-apps/bears-ts/src/db.ts
@@ -0,0 +1,29 @@
+const db = {bears: 0};
+
+// Simulated async functions
+
+export const fetchBears = (): Promise<number> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(db.bears);
+    }, 500);
+  });
+};
+
+export const increasePopulation = (): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      db.bears = db.bears + 1;
+      resolve();
+    }, 500);
+  });
+};
+
+export const removeAllBears = (): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      db.bears = db.bears = 0;
+      resolve();
+    }, 500);
+  });
+};
