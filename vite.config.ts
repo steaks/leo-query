3ed1a1f3455174e5fcea1313455a5cb77103aacc
Steven Wexler <steven.m.wexler@gmail.com>
2024-11-11 07:43:16 -0700

    Update build and create test-apps (#48)
    * Update build and create test-apps
    
    * Fix tests
    
    * Enhance CONTRIBUTING.md
    
    * Fixes to CONTRIBUTING.md
    
    * Fix test comman
    
    * Remove exclude field

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "LeoQuery",  // This is important for UMD builds
+      formats: ["es", "cjs", "umd"],  // Added UMD for direct browser usage
+      fileName: (format) => `index.${format === "es" ? "js" : format}`
+    },
+    rollupOptions: {
+      external: ["react", "zustand", "immer"],
+      output: {
+        // Globals are important for UMD builds
+        globals: {
+          react: "React",
+          zustand: "zustand",
+          immer: "immer"
+        }
+      }
+    },
+    sourcemap: true
+  },
+  plugins: [dts()]
+});
